1bdc7cc607505b74a4d840fe73cf3d7cc7ad2748
Pocol <project.asura@gmail.com>
2022-02-05 20:24:30 +0900

    修正。

--- src/asdxApp.cpp
+++ src/asdxApp.cpp
@@ -68,8 +68,8 @@ bool App::Init()
         };
 
         QuadVertex vertices[] = {
-            { glm::vec2(-1.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
-            { glm::vec2( 3.0f,  0.0f), glm::vec2(2.0f, 0.0f) },
+            { glm::vec2(-1.0f,  1.0f), glm::vec2(0.0f, 0.0f) },
+            { glm::vec2( 3.0f,  1.0f), glm::vec2(2.0f, 0.0f) },
             { glm::vec2(-1.0f, -3.0f), glm::vec2(0.0f, 2.0f) },
         };
 
